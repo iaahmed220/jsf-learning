83e971e81af2da4de668d3a7acad57e3e095a3ee
jittagornp <jittagornp@anonymous>
2014-01-13 00:49:28 +0700

    add jsf-api and jsf-web application

---
+++ jsf-api/src/main/java/com/blogspot/na5cent/jsflearning/api/App.java
@@ -0,0 +1,13 @@
+package com.blogspot.na5cent.jsflearning.api;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
